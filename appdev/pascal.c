5eba19ac16f2bb3b60797921ec6bf55d27d56c18
e2401837 <lh2954361@gmail.com>
2025-12-10 10:45:18 +0200

    New pascal.c with recursion algorithm

--- appdev/pascal.c
+++ appdev/pascal.c
@@ -7,3 +7,14 @@ int factorial(int n) {
 int n_choose_k(int n, int k) {
 	return factorial(n) / factorial(k) / factorial(n - k);
 }
+
+void recursive_pascal() {
+	const int rows = 5;
+	for (int n = 0; n < rows; n++) {
+		for (int k = 0; k <= n; k++) {
+			int val = n_choose_k(n, k);
+			printf("%d ", val);
+		}
+		printf("\n");
+	}
+}
